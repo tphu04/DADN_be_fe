6b7cf37f308657942ca43d2d696fdbea6d5681a2
tphu04 <trungphugt@gmail.com>
2025-04-17 19:30:58 +0700

    test cors

--- be-DADN-watering-system/src/server.js
+++ be-DADN-watering-system/src/server.js
@@ -13,7 +13,8 @@ const jwt = require('jsonwebtoken');
 const app = express();
 
 // Cấu hình CORS chi tiết
-const allowedOrigins = ['http://localhost:5173', 'http://127.0.0.1:5173', 'http://localhost:3000'];
+// const allowedOrigins = ['http://localhost:5173', 'http://127.0.0.1:5173', 'http://localhost:3000'];
+const allowedOrigins = ['*'];
 const corsOptions = {
     origin: allowedOrigins,
     credentials: true,
